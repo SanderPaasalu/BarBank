9fe7b7248d42e15fdcb4f1f2782c941f38d929ec
SanderPaasalu <sander.paasalu@khk.ee>
2021-10-08 06:23:36 +0300

    As a user I can register[finished #179780465]

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express');
+const app = express()
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+try {
+    const swaggerDocument = yaml.load(fs.readFileSync('swagger.yaml', 'utf8'));
+    console.log(swaggerDocument);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (e) {
+    console.log(e);
+}
+
+// Parse request body
+app.use(express.json());
+
+// Loads .env file contents into process.env
+require("dotenv").config()
+
+// Register routes
+app.use('/users', require('./routes/users'))
+
+// Open connection to MongoDB
+mongoose.connect(process.env.MONGODB_URL, function (err) {
+    if (err)
+        console.log(err);
+
+    console.log("Connected to Mongo")
+
+})
+
+// Listen for connections
+app.listen(process.env.PORT, () => {
+    console.log(`App listening at http://localhost:${process.env.PORT}`)
+})
